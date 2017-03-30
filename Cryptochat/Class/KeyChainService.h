6cae97ba6f3283acbfc9d5d6fd0c119effa8d935
Artem <ak@it-machine.ru>
2017-03-30 21:51:34 +0300

    [NIRS-32]: Add KeyChainService to project

---
+++ Cryptochat/Class/KeyChainService.h
@@ -0,0 +1,13 @@
+//
+//  KeyChainService.h
+//  Cryptochat
+//
+//  Created by Artem Konarev on 30.03.17.
+//  Copyright © 2017 Сергей Романков. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KeyChainService : NSObject
+
+@end
---
+++ Cryptochat/Class/KeyChainService.m
@@ -0,0 +1,13 @@
+//
+//  KeyChainService.m
+//  Cryptochat
+//
+//  Created by Artem Konarev on 30.03.17.
+//  Copyright © 2017 Сергей Романков. All rights reserved.
+//
+
+#import "KeyChainService.h"
+
+@implementation KeyChainService
+
+@end
