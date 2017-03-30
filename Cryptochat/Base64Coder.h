9fb535f7deaec8f704a559758baab204922e1dd2
Artem <ak@it-machine.ru>
2017-03-30 22:14:08 +0300

    [NIRS-32]: Created Base64Coder class

---
+++ Cryptochat/Base64Coder.h
@@ -0,0 +1,13 @@
+//
+//  Base64Coder.h
+//  Cryptochat
+//
+//  Created by Artem Konarev on 30.03.17.
+//  Copyright © 2017 Сергей Романков. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Base64Coder : NSObject
+
+@end
---
+++ Cryptochat/Base64Coder.m
@@ -0,0 +1,13 @@
+//
+//  Base64Coder.m
+//  Cryptochat
+//
+//  Created by Artem Konarev on 30.03.17.
+//  Copyright © 2017 Сергей Романков. All rights reserved.
+//
+
+#import "Base64Coder.h"
+
+@implementation Base64Coder
+
+@end
